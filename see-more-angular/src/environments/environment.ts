1e36c9a23a7c7516b1efe20512a1ee6d15009928
zsimmons25 <zacharysimmons25@gmail.com>
2026-01-09 22:36:25 -0800

    env change for url

--- see-more-angular/src/environments/environment.ts
+++ see-more-angular/src/environments/environment.ts
@@ -2,5 +2,5 @@ import { isDevMode } from '@angular/core';
 
 export const environment = {
   production: !isDevMode(),
-  apiUrl: isDevMode() ? 'http://localhost:3000' : '/api/seemore',
+  apiUrl: isDevMode() ? 'http://localhost:3000' : '/seemore/api',
 };
